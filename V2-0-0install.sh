8295f346ee9edc2c2a0d320998532e05d9e1686c
Aldi Fauzi <fauzine704@gmail.com>
2025-05-19 11:55:32 +0700

    Update V2-0-0install.sh

--- V2-0-0install.sh
+++ V2-0-0install.sh
@@ -5,12 +5,8 @@ pkg install git -y
 pkg install nodejs-lts -y
 pkg install ffmpeg -y
 pkg install yarn -y
-
 # Clone repository dari GitHub
 git clone https://github.com/xziyyy/clairity.git
-
-echo ""
-echo ""
-echo ""
-echo "Installasi selesai! Silakan masuk ke folder 'clairity' dengan 'cd clairity'."
-echo "lalu ketik 'yarn' setelah terinstall 'npm start'"
+cd clairity
+yarn
+npm start
