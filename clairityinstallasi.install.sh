d4687d63c3a2cc99b3422d3f70c31027aeb5b762
Aldi Fauzi <fauzine704@gmail.com>
2025-06-08 12:11:59 +0700

    Create clairityinstallasi.install.sh

---
+++ clairityinstallasi.install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install dependensi yang dibutuhkan
+pkg install git -y
+pkg install nodejs-lts -y
+pkg install ffmpeg -y
+pkg install yarn -y
+# Clone repository dari GitHub
+git clone https://github.com/xziyyy/clairity.git
+echo 'silahkan ketik:'
+echo '1. cd clairity'
+echo '2. yarn'
+echo '3. npm start'
